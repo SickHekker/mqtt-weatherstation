75d65b2a25da78591e256159208ae44fea91af53
SickHekker <Robindehek@gmail.com>
2018-04-16 16:11:41 +0200

    Update weatherstation.ino

--- weatherstation.ino
+++ weatherstation.ino
@@ -6,11 +6,20 @@
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-BME280I2C bme;
+// Deepsleep duration in seconds.
+int sleep = 300;
+
+// Set the MQTT feeds to be used
+#define temperature_feed "/outdoor/sensors/weatherstation/bme280/temperature"
+#define humidity_feed "/outdoor/sensors/weatherstation/bme280/humidity"
+#define pressure_feed "/outdoor/sensors/weatherstation/bme280/pressure"
 
 /************************* WiFi Access Point *********************************/
+
+WiFiClientSecure client;
 #define WLAN_SSID "wifissid"
 #define WLAN_PASS "wifipassword"
+
 /************************* MQTT Broker Setup *********************************/
 
 #define AIO_SERVER      "mqttserver"
@@ -18,26 +27,23 @@ BME280I2C bme;
 #define AIO_USERNAME    "mqttusername"
 #define AIO_KEY         "mqttpassword"
 
-
-WiFiClientSecure client;
+/****************************** Feeds ***************************************/
 
 Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
 
-
-/****************************** Feeds ***************************************/
-
-Adafruit_MQTT_Publish temperature_topic = Adafruit_MQTT_Publish(&mqtt, "/outdoor/sensors/weatherstation/bme280/temperature");
-Adafruit_MQTT_Publish pressure_topic = Adafruit_MQTT_Publish(&mqtt, "/outdoor/sensors/weatherstation/bme280/pressure");
-Adafruit_MQTT_Publish humidity_topic = Adafruit_MQTT_Publish(&mqtt, "/outdoor/sensors/weatherstation/bme280/humidity");
+Adafruit_MQTT_Publish temperature_topic = Adafruit_MQTT_Publish(&mqtt, temperature_feed);
+Adafruit_MQTT_Publish pressure_topic = Adafruit_MQTT_Publish(&mqtt, humidity_feed);
+Adafruit_MQTT_Publish humidity_topic = Adafruit_MQTT_Publish(&mqtt, pressure_feed);
 
 /*************************** Sketch Code ************************************/
 
-int wifitry = 0;
+BME280I2C bme;
 
 void setup() {
-
   Serial.begin(115200);
   delay(10);
+  
+  int wifitry = 0;
   
   Serial.println();
   Serial.print("Connecting to ");
@@ -50,10 +56,10 @@ void setup() {
     delay(500);
     Serial.print(".");
     wifitry++;
-    if (wifitry >= 20) {
+    if (wifitry >= 50) {
       Serial.println("Can't connect to WiFi, activating deepsleep");
       wifitry = 0;
-      ESP.deepSleep(300e6);
+      ESP.deepSleep(sleep * 1000000);
     }
   }
 
@@ -63,7 +69,7 @@ void setup() {
 
   if (!bme.begin()) {
     Serial.println("Can't find the BME280 sensor, activating deepsleep");
-    ESP.deepSleep(300e6);
+    ESP.deepSleep(sleep * 1000000);
   }
   
   MQTT_connect();
@@ -71,7 +77,7 @@ void setup() {
   float temperature(NAN), humidity(NAN), pressure(NAN);
   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
   BME280::PresUnit presUnit(BME280::PresUnit_hPa);
-   
+  
   bme.read(pressure, temperature, humidity);
  
   delay(100);
@@ -85,33 +91,26 @@ void setup() {
   temperature_topic.publish(temperature);
   humidity_topic.publish(humidity);
   pressure_topic.publish(pressure);
-  
-  delay(100);
-  
+    
   Serial.println("Data posted to MQTT");
-  
   mqtt.disconnect();
-  
   Serial.println("MQTT disconnected, activating deepsleep");
-  ESP.deepSleep(300e6);
+  ESP.deepSleep(sleep * 1000000);
   
 }
 
 void loop() {
-  ESP.deepSleep(300e6);
+  ESP.deepSleep(sleep * 1000000);
 }
 
 void MQTT_connect() {
   int8_t ret;
-
   // Stop if already connected.
   if (mqtt.connected()) {
     return;
   }
-
   Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 2;
+  uint8_t retries = 5;
   while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 1 second...");
@@ -119,9 +118,9 @@ void MQTT_connect() {
     delay(1000);  // wait 1 second
     retries--;
     if (retries == 0) {
-      ESP.deepSleep(300e6);
+      ESP.deepSleep(sleep * 1000000);
     }
   }
-
+  
   Serial.println("MQTT Connected!");
 }
